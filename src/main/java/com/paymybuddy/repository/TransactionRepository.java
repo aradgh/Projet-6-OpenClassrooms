5ec7b2c453e343d97dc30a62830e6e6380451cc8
aradgh <arad.ghorreshi@gmail.com>
2025-03-17 02:00:42 +0100

    Implementing repositories integration tests

--- src/main/java/com/paymybuddy/repository/TransactionRepository.java
+++ src/main/java/com/paymybuddy/repository/TransactionRepository.java
@@ -2,11 +2,8 @@ package com.paymybuddy.repository;
 
 import com.paymybuddy.model.Transaction;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
-import java.time.LocalDateTime;
 import java.util.List;
 
 @Repository
@@ -16,11 +13,5 @@ public interface TransactionRepository extends JpaRepository<Transaction, Long> 
     List<Transaction> findByReceiverId(Long receiverId);
 
     List<Transaction> findBySenderIdOrReceiverId(Long senderId, Long receiverId);
-
-    @Query("SELECT t FROM Transaction t WHERE t.sender.id = :userId AND t.timestamp BETWEEN :start AND :end")
-    List<Transaction> findSentTransactionsByUserIdAndPeriod(
-        @Param("userId") Long userId, @Param("start") LocalDateTime start, @Param("end") LocalDateTime end
-    );
-
 }
 
