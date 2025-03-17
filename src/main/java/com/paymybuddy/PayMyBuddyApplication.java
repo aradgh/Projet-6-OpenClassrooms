0ef037f28217a2ec0a90aa65d5e6ee7bfe8a6897
aradgh <arad.ghorreshi@gmail.com>
2025-03-17 03:01:53 +0100

    Fix application.properties

--- src/main/java/com/paymybuddy/PayMyBuddyApplication.java
+++ src/main/java/com/paymybuddy/PayMyBuddyApplication.java
@@ -1,5 +1,6 @@
 package com.paymybuddy;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,6 +8,15 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class PayMyBuddyApplication {
 
     public static void main(String[] args) {
+        // Charger le fichier .env
+        Dotenv dotenv = Dotenv.configure()
+            .ignoreIfMissing()  // Pour ne pas planter si le fichier n'existe pas
+            .load();
+
+        // Optionnel : transférer les variables dans les propriétés système
+        System.setProperty("DB_USERNAME", dotenv.get("DB_USERNAME"));
+        System.setProperty("DB_PASSWORD", dotenv.get("DB_PASSWORD"));
+
         SpringApplication.run(PayMyBuddyApplication.class, args);
     }
 
